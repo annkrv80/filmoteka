7b3174f97f9ca323bd79ce8974db6aa0be27ad49
Anna <annkrv80@yandex.ru>
2023-02-05 19:42:16 +0700

    Main

---
+++ Main.java
@@ -0,0 +1,11 @@
+import java.io.IOException;
+import java.text.ParseException;
+
+import Controller.Controller;
+
+public class Main {  
+    public static void main(String[] args) throws IOException, ParseException  {
+        Controller controller=new Controller();
+        controller.start();    
+    }
+}
